eaba7bed06c68c431d18f61f1334dfb32ec3e427
MaRloR64 <eyk230679@gmail.com>
2024-10-18 18:22:58 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+    // Таблица умножения на 7
+    std::cout << "Таблица умножения на 7:" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " x 7 = " << i * 7 << std::endl;
+    }
+
+    // Ввод числа N с клавиатуры
+    int N;
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    // Таблица умножения на N
+    std::cout << "Таблица умножения на " << N << ":" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " x " << N << " = " << i * N << std::endl;
+    }
+
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    // Таблица умножения на 7
+    std::cout << "Таблица умножения на 7:" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " x 7 = " << i * 7 << std::endl;
+    }
+
+    // Ввод числа N с клавиатуры
+    int N;
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    // Таблица умножения на N
+    std::cout << "Таблица умножения на " << N << ":" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " x " << N << " = " << i * N << std::endl;
+    }
+
+    return 0;
+}
